478f0cd6a5de303f3eeb9a8f4c2fe98afc7aacf2
Anderson Júnior <andersonjuniorpram@gmail.com>
2025-06-13 13:11:36 -0300

    projeto conta banco finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        var agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da Conta !");
+        var numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente !");
+        var nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo !");
+        var saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nome +  
+        ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de " + saldo + 
+        " já está disponível para saque");
+        
+        scanner.close();
+
     }
+
 }
